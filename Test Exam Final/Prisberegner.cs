f2def669ec903e39c8785df87355e30a1c02e26e
emil dr <emil-rere@live.dk>
2023-05-17 12:12:35 +0200

    lidt kode

---
+++ Test Exam Final/Prisberegner.cs
@@ -0,0 +1,37 @@
+﻿namespace Test_Exam_Final
+{
+    public class Prisberegner
+    {
+
+        static void CalculatePrice(int distance)
+        {
+
+            int kmprice = 500; //starting fee
+
+            if (distance < 100)
+            {
+                kmprice += distance * 20;
+            } else if (distance < 200)
+            {
+                kmprice += (distance-100) * 15;
+                kmprice += 2000;
+            } else if (distance < 500)
+            {
+                kmprice = (distance - 200) * 10;
+                kmprice += 3500;
+            }
+
+            Console.WriteLine(kmprice);
+
+            //price1 = 100
+            //price2 = 
+            //price3 = 
+
+            //modes: 
+            //No multiplier = 000
+            //Imprægnering  = 100
+            //DoubleVask    = 010
+            //Both          = 001
+        }
+    }
+}
